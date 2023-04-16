b2807f4276f4bc7b17d5f17f9d8a152b89229acc
Abdalrhman Abu-Sbeit <abdalrhman@Abdalrhmans-MacBook-Pro.local>
2023-04-16 23:17:32 +0200

    fix gas bug and add simulator for gas price and add the jar file

--- cargo/src/main/java/com/bdma/cargo/model/GasModel.java
+++ cargo/src/main/java/com/bdma/cargo/model/GasModel.java
@@ -3,14 +3,16 @@ package com.bdma.cargo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class GasModel {
 
     String currency;
-    String lpg;
-    String diesel;
-    String gasoline;
+    BigDecimal lpg;
+    BigDecimal diesel;
+    BigDecimal gasoline;
     String country;
 
 }
