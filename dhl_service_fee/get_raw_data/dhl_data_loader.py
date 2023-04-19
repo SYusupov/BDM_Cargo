d2c5a6aa620229f460b504a45c8b24a137eb826f
997-icu <245173011@qq.com>
2023-04-19 02:54:19 +0200

    DHL Data Loader to Temporary Landing

--- dhl_service_fee/get_raw_data/dhl_data_loader.py
+++ dhl_service_fee/get_raw_data/dhl_data_loader.py
@@ -21,14 +21,10 @@ def dhl_raw_data_loader(url):
         file = cursor.content
         return file
         
-
-
-
-
 if __name__ == "__main__":
     Domestic_Price_url = "https://raw.githubusercontent.com/SYusupov/BDM_Cargo/main/dhl_service_fee/raw_data/Domestic_Price.txt" 
     Domestic_Zona_url = "https://raw.githubusercontent.com/SYusupov/BDM_Cargo/main/dhl_service_fee/raw_data/Domestic_Zona.csv"
-    Zona_Matrix_url = "https://github.com/SYusupov/BDM_Cargo/blob/b069c2da856b9999b3c668e1503eb7f9c8880a6e/dhl_service_fee/raw_data/Zona_Matrix.xlsx"
+    Zona_Matrix_url = "https://raw.githubusercontent.com/SYusupov/BDM_Cargo/main/dhl_service_fee/raw_data/Zona_Matrix.csv"
 
     DP = dhl_raw_data_loader(Domestic_Price_url)
     DZ = dhl_raw_data_loader(Domestic_Zona_url)
@@ -36,10 +32,16 @@ if __name__ == "__main__":
     
     DP_df = pd.read_csv(io.StringIO(DP.decode('utf-8')),sep=" ",names = ["Peso(en kg)", "A", "B","C","D"],skiprows=[0])
     DZ_df = pd.read_csv(io.StringIO(DZ.decode('utf-8')),sep=",")
-    ZM_df = pd.read_csv(io.StringIO(ZM.decode('utf-8')))
+    ZM_df = pd.read_csv(io.StringIO(ZM.decode('utf-8')),sep = ',')
     
 
 
-    print(DP_df)
-    print(DZ_df)
-    print(ZM_df)
+    print(DP_df.head(),"\n")
+    print(DZ_df.head(),"\n")
+    print(ZM_df.head(),"\n")
+    
+    DP_df.to_csv("Domestic_Price.csv",index = False,encoding = "utf_8_sig")
+    DZ_df.to_csv("Domestic_Zona.csv",index = False,encoding = "utf_8_sig")
+    ZM_df.to_csv("Zona_Matrix.csv",index = False,encoding = "utf_8_sig")
+    
+    
