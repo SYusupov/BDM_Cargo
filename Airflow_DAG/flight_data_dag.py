a39f60ebfaff9e7f539ef852bf510ab670d95c81
xuyou1999 <yx1711@nyu.edu>
2023-04-22 18:12:57 +0200

    update flight dag and output

--- Airflow_DAG/flight_data_dag.py
+++ Airflow_DAG/flight_data_dag.py
@@ -3,62 +3,63 @@ from datetime import date, timedelta, datetime
 import json
 import csv
 import time
+import pandas as pd
+import random
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
 def get_flight_data():
+    directs = "MAD-BCN, MAD-PMI, MAD-AGP, MAD-ALC, BCN-MAD, BCN-PMI, BCN-AGP, BCN-ALC, PMI-BCN, PMI-MAD, PMI-AGP, PMI-ALC, AGP-MAD, AGP-BCN, AGP-PMI, ALC-BCN, ALC-MAD, ALC-PMI".split(", ")
+    directs = [x for x in directs if 'ALC' not in x]
+    directs = [x.split("-") for x in directs]
+
     days = 7
-    for i in range(1, days+1):
-        cur_date = date.today() + timedelta(days=i)
-        print(cur_date)
+    req_params = ['flightNumber', 'departureAirportFsCode', 'arrivalAirportFsCode', 'departureTime', 'arrivalTime', 'stops', 'departureTerminal', 'arrivalTerminal']
+    timestamp = int(time.time())
+    file_name = f'logs/scheduledFlights_4dirs_{timestamp}.csv'
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(req_params)
+    
+    for direct in directs:
+        for i in range(1, days+1):
+            cur_date = date.today() + timedelta(days=i)
 
-        # scheduled flights by route, departing on the given date
-        params = {
-            'appId':'8d9f59d5',
-            'appKey':'434d9d90d914eef00c84cad6add63688',
-            'departureAirportCode':'BCN',
-            'arrivalAirportCode':'MAD',
-            'year':cur_date.year,
-            'month':cur_date.month,
-            'day':cur_date.day,
-            'codeType':'IATA'
-        }
+            params = {
+                'appId':'8d9f59d5',
+                'appKey':'434d9d90d914eef00c84cad6add63688',
+                'departureAirportCode':direct[0],
+                'arrivalAirportCode':direct[1],
+                'year':cur_date.year,
+                'month':cur_date.month,
+                'day':cur_date.day,
+                'codeType':'IATA'
+            }
 
-        api_result = requests.get(
-            "https://api.flightstats.com/flex/schedules/rest/v1/json"
-            f"/from/{params['departureAirportCode']}/to/{params['arrivalAirportCode']}"
-            f"/departing/{params['year']}/{params['month']}/{params['day']}"
-            f"?appId={params['appId']}"
-            f"&appKey={params['appKey']}"
-            f"&codeType={params['codeType']}")
-        
-        output = api_result.json()
+            api_result = requests.get(
+                "https://api.flightstats.com/flex/schedules/rest/v1/json"
+                f"/from/{params['departureAirportCode']}/to/{params['arrivalAirportCode']}"
+                f"/departing/{params['year']}/{params['month']}/{params['day']}"
+                f"?appId={params['appId']}"
+                f"&appKey={params['appKey']}"
+                f"&codeType={params['codeType']}")
+            
+            output = api_result.json()
 
-        # with open("response2_FlightStats_SchedulesAPI.json", "w") as out:
-        # json.dump(output, out)
-
-        req_params = ['flightNumber', 'departureAirportFsCode', 'arrivalAirportFsCode', 'departureTime', 'arrivalTime', 'stops', 'departureTerminal', 'arrivalTerminal']
-        timestamp = int(time.time())
-        with open(f'logs/scheduledFlights_{cur_date}_{timestamp}.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            # writer.writerow(req_params)
-            for idx in range(len(output['scheduledFlights'])):
-                writer.writerow([output['scheduledFlights'][idx][param] for param in req_params])
-
-    params = {
-    'access_key': 'e78c58f49ec83555fef1d339c0cc83ee',
-    'dep_iata': 'BCN',
-    'arr_iata': 'MAD'
-    }
-
-    # request 1, param: just access_key
-    # api_result = requests.get('https://api.aviationstack.com/v1/flights', params)
-
-    # request 2
-    api_result = requests.get('http://api.aviationstack.com/v1/routes', params)
-    api_response = api_result.json()
-    print(api_response)
+            
+            with open(file_name, 'a', newline='') as file:
+                writer = csv.writer(file)
+                for idx in range(len(output['scheduledFlights'])):
+                    output_row = []
+                    for param in req_params:
+                        if param in output['scheduledFlights'][idx]:
+                            output_row.append(output['scheduledFlights'][idx][param])
+                        else:
+                            output_row.append(None)
+                    writer.writerow(output_row)
+    df = pd.read_csv(file_name)
+    df.to_parquet(f'logs/scheduledFlights_4dirs_{timestamp}.parquet')
 
 default_args = {
     "owner": "airflow",
